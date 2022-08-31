0c8e7aa0e253394d18ee97d203e72c4936d91cdb
Abelino Sepulveda Estrada <60146746+asepulvede@users.noreply.github.com>
2022-08-31 18:23:19 -0500

    Create regla_falsa.py

---
+++ Raices/regla_falsa.py
@@ -0,0 +1,33 @@
+!pip install sympy
+import sympy as sp
+import numpy as np
+from sympy import *
+import math
+from math import *
+x= symbols('x')
+
+def regla_falsa(fx,x0,xf,maxI,tol):
+  if fx.subs(x,x0)*fx.subs(x,xf)==0:
+    print(x0, '0', xf, 'son raiz')
+  elif fx.subs(x,x0)*fx.subs(x,xf)>0:
+    print('no hay raiz')
+  else:
+    xm= xf- fx.subs(x,xf)*(x0-xf)/(fx.subs(x,x0)-fx.subs(x,xf))
+    cont=0
+    error= abs(x0-xm)
+    while error>tol and maxI>cont and fx.subs(x,xm)!=0:
+      if fx.subs(x,x0)*fx.subs(x,xm)<0:
+        xf=xm 
+      else:
+        xi=xm 
+      xm= xf- fx.subs(x,xf)*(x0-xf)/(fx.subs(x,x0)-fx.subs(x,xf))
+      error= abs(x0-xm)
+      cont= cont+1
+    if fx.subs(x,xm)==0:
+      print(xm,'es raiz')
+    elif error<tol:
+      print(xm,'es raiz con tolerancia',tol)
+    else:
+      print('no hay solución')
+
+regla_falsa(x**2-1,0,2,20,0.05)
